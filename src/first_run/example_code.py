57af5cb88c3f621e8fd6165abc5538c082ceca46
Fahad <fahad.butt10@gmail.com>
2022-03-13 11:20:26 +0100

    correct errors

--- src/first_run/example_code.py
+++ src/first_run/example_code.py
@@ -1,2 +1,7 @@
 def capitalize_name(name: str) -> str:
-    return print(name.capitalize())
+    capitalized_name = name.capitalize()
+    return capitalized_name
+
+
+def uppercase_name(name: str) -> str:
+    return name.upper()
--- tests/test_first_run.py
+++ tests/test_first_run.py
@@ -1,3 +1,12 @@
-def test_capitalize():
-    assert 'david' == 'David'
-    assert 'fahad' == 'Fahad'
+import pytest
+from src.first_run.example_code import capitalize_name, uppercase_name
+
+
+def test_capitalize_name():
+    assert capitalize_name('david') == 'David'
+    assert capitalize_name('fahad') == 'Fahad'
+
+
+def test_uppercase_name():
+    assert uppercase_name('david') == 'DAVID'
+    assert uppercase_name('fahad') == 'FAHAD'
