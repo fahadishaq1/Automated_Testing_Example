f0eb3241177d1d47ebfb3d486adf38825d5f86f6
Fahad Ishaq <fahad.butt10@gmail.com>
2022-03-13 15:52:52 +0100

    add new test

--- tests/test_first_run.py
+++ tests/test_first_run.py
@@ -1,5 +1,5 @@
 import pytest
-from src.first_run.example_code import capitalize_name, uppercase_name
+from src.first_run.example_code import capitalize_name, uppercase_name, foo
 
 
 def test_capitalize_name():
@@ -10,3 +10,8 @@ def test_capitalize_name():
 def test_uppercase_name():
     assert uppercase_name('david') == 'DAVID'
     assert uppercase_name('fahad') == 'FAHAD'
+
+
+@pytest.mark.parametrize("number", [1, 2, 3, 42])
+def test_foo(number):
+    assert foo(number) > 0
